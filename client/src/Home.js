9f545409c802aaaa5c17409b95be43408bc5850e
AhmedShab <ahmed.vuw@gmail.com>
2018-08-03 00:11:08 +0300

    done with home page

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,12 +1,35 @@
-import React, { Component } from 'react';
-import background from './images/background.jpg';
-// import background from './images/background.jpg';
+import React, { Component, Fragment } from 'react';
+import homeIcone from './images/icons/homeIcone.jpg';
+import infoIcon from './images/icons/infoIcon.jpg';
+import loginIcon from './images/icons/loginIcon.jpg';
+import lostIcon from './images/icons/lostIcon.jpg';
+import qrCodeIcon from './images/icons/qrCodeIcon.jpg';
+import callusIcon from './images/icons/callusIcon.jpg';
+import whoareWeIcon from './images/icons/whoareWeIcon.jpg';
+import login from './images/login.jpg';
 
 export default class Home extends Component {
  render() {
   return (
-   <div>Home</div>
-   
+   <Fragment>
+    <div class="homepage">
+     <div className="btnhome">
+      <img src={login} className="" alt="Cinque Terre" />
+     </div>
+    </div>
+    <div class="icons">
+     <img src={qrCodeIcon} className="Home-img" alt="Cinque Terre" />
+     <img src={lostIcon} className="Home-img" alt="Cinque Terre" />
+     <img src={loginIcon} className="Home-img" alt="Cinque Terre" />
+
+    </div>
+    <div class="icons">
+     <img src={callusIcon} className="Home-img" alt="Cinque Terre" />
+     <img src={whoareWeIcon} className="Home-img" alt="Cinque Terre" />
+     <img src={infoIcon} className="Home-img" alt="Cinque Terre" />
+
+    </div>
+   </Fragment>
   )
  }
 }
--- client/src/SelectOptions.js
+++ client/src/SelectOptions.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import background from './images/background.jpg';
-import QRImg from './images/QR.jpg';
+// import QRImg from './images/QR.jpg';
 import lostImg from './images/lost.jpg';
 import infoImg from './images/info.jpg';
 
@@ -8,7 +8,7 @@ export default class SelectOptions extends Component {
  render() {
   return (
    <div className="SelectOptions-container">
-    <button type="button" className="btn"><img className="btn-img img-responsive" src={QRImg} alt="" /></button>
+    {/* <button type="button" className="btn"><img className="btn-img img-responsive" src={QRImg} alt="" /></button> */}
     <button type="button" className="btn"><img className="btn-img img-responsive" src={lostImg} alt="" /></button>
     <button type="button" className="btn"><img className="btn-img img-responsive" src={infoImg} alt="" /></button>
    </div>
